785be3f632665f1636831992d5998ddba638f72f
Baptiste Rosselet <baptiste.rosselet@protonmail.com>
2025-11-15 19:12:33 -0500

    feat(subscription): Creating /subscriptions endpoint skeleton with url-encoded form parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,35 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, mime::Name, web,
+};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn subscribe(form: web::Form<FormData>) -> impl Responder {
+    //let name = req.match_name();
+    print!(
+        "The new user is {} and his mail is {}.",
+        form.name, form.email
+    );
+    HttpResponse::Ok()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,13 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
+    let port = listener.local_addr().unwrap().port();
+    let server_task = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server_task);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[actix_web::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -7,7 +15,7 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &address))
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute the request.");
@@ -16,10 +24,47 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
-    let port = listener.local_addr().unwrap().port();
-    let server_task = zero2prod::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server_task);
-    format!("http://127.0.0.1:{}", port)
+#[actix_web::test]
+async fn subscribe_returns_a_200_for_valid_data_form() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guind&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", app_address))
+        .header("Content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_web::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", app_address))
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
